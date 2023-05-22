cd93b377071c74b14d5db85f6f0e61942e185102
pdhakal906 <pdhakal906@gmail.com>
2023-05-22 16:49:05 +0545

    changes

--- main_report_list.js
+++ main_report_list.js
@@ -1,6 +1,6 @@
 var storedData = localStorage.getItem("formData");
 var allFormData = JSON.parse(storedData) || [];
-
+console.log("hello");
 // Function to get all IDs, names, addresses, and phone numbers
 function getAllResult() {
   var ids = [];
@@ -28,27 +28,34 @@ function getAllResult() {
 var result = getAllResult();
 var ids = result.ids;
 var names = result.names;
-var addresses = result.names;
+var addresses = result.addresses;
 var phone = result.phoneNumbers
 var table = document.getElementById("data-table");
 
 for (var i = 0; i < ids.length; i++) {
-  var html = "<tr><td id='house-number'>" + ids[i] + "</td><td id = 'name'><a href='family_report.html?house=" + ids[i] + "'>" + names[i] + "</a></td><td id='address'>" + addresses[i] + "</td><td><a class = 'btn btn-danger' href = 'main_report_list.htnl' id = 'delete'>X</a></td></tr>";
+  var html = "<tr><td id='house-number'>" + ids[i] + "</td><td id = 'name'><a href='family_report.html?house=" + ids[i] + "'>" + names[i] + "</a></td><td id='address'>" + addresses[i] + "</td><td><a class = 'btn btn-danger' href = 'report_list.html' id = 'delete'>X</a></td></tr>";
 
   table.innerHTML += html;
 }
-id = document.getElementById("house-number").value;
 
 var delete_button = document.getElementById("delete");
-delete_button.addEventListener("click", function deleteFam);
-function deleteFam(id) {
-  var index = data.findIndex(function)
+function deleteFam() {
+  var delId = document.getElementById("house-number").innerText;
+  var index = allFormData.findIndex(function (item) {
+    return item.id === delId;
+  });
+
+  if (index !== -1) {
+    allFormData.splice(index, 1);
+    console.log(`Data with ID ${delId} deleted successfully.`);
+    localStorage.setItem("formData", JSON.stringify(allFormData));
+  }
+  else {
+    console.log(`Data with ID ${delId} not found.`)
+  }
 
 }
 
+delete_button.addEventListener("click", deleteFam);
 
-// console.log("All IDs:", result.ids);
-// console.log("All Names:", result.names);
-// console.log("All Addresses:", result.addresses);
-// console.log("All Phone Numbers:", result.phoneNumbers);
 
